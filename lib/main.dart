7ab61a3829612b092de7c73045ce80ee18ebbac3
Nilaina Kezia <112585159+kezi-stack@users.noreply.github.com>
2026-09-10 15:06:31 +0200

    Add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'presentation/providers/app_providers.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: EscapadesApp()));
+}
+
+class EscapadesApp extends ConsumerWidget {
+  const EscapadesApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+    return MaterialApp.router(
+      title: 'Escapades',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: themeMode,
+      routerConfig: appRouter,
+    );
+  }
+}
